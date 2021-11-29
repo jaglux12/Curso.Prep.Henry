46a3ba7656af838955bdf80f04e7dbabd0e9ff2f
jaglux12 <gonzalopez239@gmail.com>
2021-11-29 20:46:41 -0300

    ejercio terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -75,7 +75,7 @@ function tienenMismaLongitud(str1, str2) {
   // De lo contrario, devuelve "false"
   // Tu código:
   var res
-  if (str1.lenght == str2.lenght){
+  if (str1.length == str2.length){
     res = true
   }
   else{
@@ -123,7 +123,7 @@ function esPar(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
   var res
-  if ((num%2)==1){
+  if ((num%2)==0){
     res = true
   }
   else{
@@ -137,7 +137,7 @@ function esImpar(num) {
   // De lo contrario, devuelve "false"
   // Tu código:
   var res
-  if ((num%2)!=1){
+  if ((num%2)!=0){
     res = true
   }
   else{
@@ -218,27 +218,27 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola " + nombre
+  return "Hola " + nombre+"!"
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-
+return alto * ancho
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-
+return lado + lado + lado + lado
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  return (base*altura)/2
 }
 
 
@@ -246,7 +246,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-
+  return 1.20 * euro
 }
 
 
@@ -256,7 +256,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-
+  var res
+  if ("aeiou".search(letra) != -1)
+  {
+    res = "Es vocal"
+  }else {
+    res = "Dato incorrecto"
+  }
+  return res
 }
 
 
