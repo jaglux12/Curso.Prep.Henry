048ef3edcfdd8b3238cd3fc91229d52f263a627e
jaglux12 <gonzalopez239@gmail.com>
2021-11-30 19:53:06 -0300

    teoría resuelta del modulo 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -232,6 +232,7 @@ function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
 return lado + lado + lado + lado
+
 }
 
 
